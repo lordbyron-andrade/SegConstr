3d6353b49d73ee001381942bc6b1a7d48a794c2f
luis <nwixoye@hotmail.com>
2019-09-11 21:34:15 -0500

    Base del proyecto

---
+++ app/src/main/java/com/kgm/segconstr/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kgm.segconstr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Button bLogin;
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bLogin = findViewById(R.id.button);
+
+
+
+
+    }
+
+    public void manejoLogin(View view) {
+        Toast.makeText(this, "Login",Toast.LENGTH_SHORT).show();
+    }
+}
