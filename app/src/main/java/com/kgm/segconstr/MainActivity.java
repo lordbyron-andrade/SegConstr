050914d7f4bed8cc5f35ad26c93c6211258ca56f
luis <nwixoye@hotmail.com>
2019-09-11 22:08:14 -0500

    Base del proyecto

--- app/src/main/java/com/kgm/segconstr/MainActivity.java
+++ app/src/main/java/com/kgm/segconstr/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kgm.segconstr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +26,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void manejoLogin(View view) {
-        Toast.makeText(this, "Login",Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/com/kgm/segconstr/TomaFoto.java
@@ -0,0 +1,13 @@
+package com.kgm.segconstr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TomaFoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toma_foto);
+    }
+}
